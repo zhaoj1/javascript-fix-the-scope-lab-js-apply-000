d363f30a62acf628bdeb77eef7426d58ca63dad6
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-19 18:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
